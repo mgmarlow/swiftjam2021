8bcffb1b6dbb92646085056c525d02f09c691c8e
Graham Marlow <mgmarlow@hey.com>
2021-11-14 14:00:27 -0800

    Swap out entities array for EntityManager, remove old Entity class

--- gamejam/GameScene.swift
+++ gamejam/GameScene.swift
@@ -6,60 +6,59 @@ protocol GameObject {
 }
 
 class GameScene: SKScene {
-    var entities: [GameObject] = []
     var levelLoader: TilemapLoader?
+    var entityManager: EntityManager?
+    var lastUpdateTimeInterval: TimeInterval = 0
     
-    private var player: Player?
-    
-    // Prefer sceneDidLoad or didMove for initialization? Original uses didMove.
-    override func sceneDidLoad() {
-        self.levelLoader = TilemapLoader(fileNamed: "level_1")
-        let tilemap = self.levelLoader!.createTilemapNode()
+    override func didMove(to view: SKView) {
+        entityManager = EntityManager(scene: self)
+        levelLoader = TilemapLoader(fileNamed: "level_1")
+        
+        let tilemap = levelLoader!.createTilemapNode()
         tilemap.anchorPoint = .zero
-        self.addChild(tilemap)
-        self.levelLoader!.forEachEntity(self.createEntities)
+        addChild(tilemap)
+        levelLoader?.forEachEntity(createEntities)
     }
     
     override func keyDown(with event: NSEvent) {
-        self.entities.forEach({ e in
-            e.handleKeyDown(event)
-        })
+        entityManager?.handleKeyDown(event)
     }
     
-    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let deltaTime = currentTime - lastUpdateTimeInterval
+        lastUpdateTimeInterval = currentTime
+        entityManager?.update(deltaTime)
     }
     
     func createEntities(obj: Object) {
-        if let entity = self.getEntityFromObject(obj) {
-            self.addChild(entity)
-            self.entities.append(entity)
+        if let entity = getEntityFromObject(obj) {
+            entityManager?.add(entity)
         }
     }
     
-    func getEntityFromObject(_ obj: Object) -> Entity? {
-        let tilemap = self.levelLoader!.tilemap!
+    func getEntityFromObject(_ obj: Object) -> GKEntity? {
+        let tilemap = levelLoader!.tilemap!
         // Correct Y coordinate due to SpriteKit's bottom-left origin
         let correctedY = tilemap.height * tilemap.tileheight - obj.y
-        
+
         switch obj.name {
-        // We'll probably pass this through as default, and lookup entity-specific properties
-        // based on the entity name in a separate class.
         case "roach", "simple_trap":
-            let entity = Entity(self, size: CGSize(width: 64, height: 64), imageNamed: obj.name)
-            // TODO: implement as entity#setPositionFromPx to create cx, cy (from player class)
-            entity.name = obj.name
-            entity.position.x = CGFloat(obj.x)
-            entity.position.y = CGFloat(correctedY)
-            entity.anchorPoint = .zero
+            // TODO: Move these into their own object classes
+            let entity = GKEntity()
+            entity.addComponent(SpriteComponent(texture: SKTexture(imageNamed: obj.name)))
+            if let sprite = entity.component(ofType: SpriteComponent.self) {
+                sprite.node.name = obj.name
+                sprite.node.position.x = CGFloat(obj.x)
+                sprite.node.position.y = CGFloat(correctedY)
+                sprite.node.anchorPoint = .zero
+            }
             return entity
         case "player":
-            return Player(self, x: obj.x, y: correctedY)
+            return Player(x: obj.x, y: correctedY)
         default:
             print("unhandled obj: \(obj.name)")
         }
-        
+
         return nil
     }
 }
