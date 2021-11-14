3f3e7c24e072086c9f00c606b73ff188334eb664
Graham Marlow <mgmarlow@hey.com>
2021-11-14 13:59:07 -0800

    Add new EntityManager class to hold all of the game's entities

---
+++ gamejam/EntityManager.swift
@@ -0,0 +1,39 @@
+import Foundation
+import GameplayKit
+import SpriteKit
+
+class EntityManager {
+    var entities = Set<GKEntity>()
+    let scene: SKScene
+    lazy var moveSystem: GKComponentSystem<MoveComponent> = GKComponentSystem(componentClass: MoveComponent.self)
+    
+    init(scene: SKScene) {
+        self.scene = scene
+    }
+    
+    func add(_ entity: GKEntity) {
+        entities.insert(entity)
+        
+        if let spriteNode = entity.component(ofType: SpriteComponent.self)?.node {
+            scene.addChild(spriteNode)
+        }
+        
+        moveSystem.addComponent(foundIn: entity)
+    }
+    
+    func remove(_ entity: GKEntity) {
+        if let spriteNode = entity.component(ofType: SpriteComponent.self)?.node {
+            spriteNode.removeFromParent()
+        }
+        
+        entities.remove(entity)
+    }
+    
+    func handleKeyDown(_ event: NSEvent) {
+        moveSystem.components.forEach({ $0.handleKeyDown(event) })
+    }
+    
+    func update(_ deltaTime: CFTimeInterval) {
+        moveSystem.components.forEach({ $0.update(deltaTime: deltaTime) })
+    }
+}
